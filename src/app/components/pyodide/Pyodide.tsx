8d55f18c6bce1d1c2d48bec76ec8360cc8bac6c2
Sen-Vmh <r0982669@student.thomasmore.be>
2026-04-10 15:06:44 +0200

    refactor (pyodide): extract token table to standalone component

--- src/app/components/pyodide/Pyodide.tsx
+++ src/app/components/pyodide/Pyodide.tsx
@@ -2,6 +2,7 @@
 
 import CodeBlock from "@/app/components/pyodide/CodeBlock";
 import StatusBar from "@/app/components/pyodide/StatusBar";
+import TokenTable from "@/app/components/pyodide/TokenTable";
 import { usePyodide } from "@/hooks/usePyodide";
 import { TOKENIZER_SCRIPT } from "@/utils/pythonScripts";
 import { useEffect, useState } from "react";
@@ -66,35 +67,7 @@ export default function Pyodide() {
             { error ? <StatusBar type="alert" className="mt-3">Error: cannot tokenize</StatusBar> : 
               tokens.length == 0 ? <StatusBar type="info" className="mt-3">Loading...</StatusBar> : 
               
-                <div className="mt-4 overflow-x-auto">
-                    <table className="w-full text-left border-collapse">
-                        <thead>
-                            <tr className="border-b-2 border-gray-700">
-                                <th className="py-3 pr-4 font-semibold text-lg">Token</th>
-                                <th className="py-3 px-4 font-semibold text-lg">Token Exact (Integer)</th>
-                                <th className="py-3 pl-4 font-semibold text-lg">Value</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody className="divide-y divide-gray-800">
-                            {tokens.map((token, index) => (
-                                <tr key={index} className="hover:bg-gray-100 transition-colors">
-                                    <td className="py-2 pr-4 align-middle">
-                                        <span className="px-2  py-1 text-gray-400 text-sm border rounded bg-gray-100 dark:bg-gray-800 border-gray-300 dark:border-gray-600">
-                                            {token.exact_type_name}
-                                        </span>
-                                    </td>
-                                    <td className="py-2 px-4 align-middle text-gray-600 dark:text-gray-400">
-                                        {token.exact_type}
-                                    </td>
-                                    <td className="py-2 pl-4 align-middle font-mono text-sm">
-                                        {JSON.stringify(token.string)}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                <TokenTable tokens={tokens} />
             }
         </>
     );
